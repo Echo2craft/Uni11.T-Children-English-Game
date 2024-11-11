71543c4dd8144051961b6292c263c9c40706638f
Echo2craft <justechojepar@gmail>
2024-11-11 12:01:07 +0700

    fix some pages merge bug

--- CEG_RazorWebApp/Pages/Student/Class/ClassInfo.cshtml.cs
+++ CEG_RazorWebApp/Pages/Student/Class/ClassInfo.cshtml.cs
@@ -1,12 +1,18 @@
+using CEG_RazorWebApp.Models.Class.Update;
+using CEG_RazorWebApp.Models.Schedule.Create;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace CEG_RazorWebApp.Pages.Student.Class
 {
     public class ClassInfoModel : PageModel
     {
-        public void OnGet()
+        public int? ClassID;
+        public void OnGet(
+            [FromRoute][Required] int classId)
         {
+            ClassID = classId;
         }
     }
 }
