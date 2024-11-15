811c4e43d6b9e274a0b2a2f947ea6ba39102b64b
Edwin Bùi Hữu Vinh <bhvinh.brv@gmail.com>
2024-11-15 20:21:43 +0700

    up working vnpay flow, updating create transaction and enroll api

---
+++ CEG_BAL/ViewModels/Transaction/CreateTransaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEG_BAL.ViewModels.Transaction
+{
+    public class CreateTransaction
+    {
+        public string ParentFullname { get; set; } = null!;
+        public string VnpayId { get; set; } = null!;
+        public int TransactionAmount { get; set; }
+        public string TransactionType { get; set; } = null!;
+    }
+}
---
+++ CEG_BAL/ViewModels/Transaction/TransactionRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEG_BAL.ViewModels.Transaction
+{
+    public class TransactionRequest
+    {
+        public string? ParentFullname { get; set; }
+        public int TransactionAmount { get; set; }
+        public string TransactionType { get; set; } = null!;
+    }
+}
