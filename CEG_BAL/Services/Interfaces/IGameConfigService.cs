c1441d80860cba10e0015edbc6443a07c866f1c5
Echo2craft <justechojepar@gmail>
2024-11-29 19:16:47 +0700

    add game config api

--- CEG_BAL/Services/Interfaces/IGameConfigService.cs
+++ CEG_BAL/Services/Interfaces/IGameConfigService.cs
@@ -1,4 +1,5 @@
 ﻿using CEG_BAL.ViewModels;
+using CEG_BAL.ViewModels.Admin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,9 @@ namespace CEG_BAL.Services.Interfaces
 {
     public interface IGameConfigService
     {
-        void Create(GameConfigViewModel model);
+        void Create(CreateNewGameConfig newGameConfig);
         void Update(GameConfigViewModel model);
         Task<List<GameConfigViewModel>> GetGameConfigsList();
-        Task<GameConfigViewModel> GetGameConfigById(int id);
+        Task<GameConfigViewModel?> GetGameConfigById(int id);
     }
 }
